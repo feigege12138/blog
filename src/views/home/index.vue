1910b16134782eba86e2fb3db908de98be083771
feigege12138 <417667763@qq.com>
2021-03-18 15:49:57 +0800

    day1

---
+++ src/views/home/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="content">
+    home
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  created(){
+
+  },
+  mounted(){
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style lang="scss" >
+*{
+  padding: 0;
+  margin: 0;
+}
+ 
+
+</style>
